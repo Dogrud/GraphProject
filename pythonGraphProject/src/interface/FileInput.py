a707e74c43d846398f3f1877a9820b29baf6e123
Mirac <miracciftci.cs@gmail.com>
2023-05-26 13:44:34 +0300

    Update FileInput.py

--- pythonGraphProject/src/interface/FileInput.py
+++ pythonGraphProject/src/interface/FileInput.py
@@ -144,7 +144,6 @@ class Ui_Form(QMainWindow):
     
     def open_dialog_box_model(self):
         self.doldur()
-        self.saveToFile()
         self.getGraph()
     
     def open_dialog_box_treshold(self):
@@ -231,21 +230,6 @@ class Ui_Form(QMainWindow):
             print(f"{node.textNo}) puan = {node.textPoint} - {node.text}")
             for i in range(len(node.nodeBenzerlikleri)):
                 print(f"{node.textNo} cumle - {i} cumle benzerligi =  {node.nodeBenzerlikleri[i]}")
-
-    def saveToFile(self):
-        service = sc.Service()
-        ozet = service.textOzetleme(Input.nodes)
-        rougeSkoru = service.textOzetlemeROUGEskoru(ozet,Input.textOzet)
-        f = open("metinOzeti.txt", "w")
-        f.write("Metin Ozeti ;\n")
-        f.write(ozet)
-        f.write(f"\n\nROUGE benzerlik skoru: {rougeSkoru}")
-        f.close()
-
-        file = open("cumleler.txt","w")
-        for node in Input.nodes:
-            file.write(f"{node.textNo} - {node.text}\n")
-        file.close()
 
 if __name__ == "__main__":
     import sys
